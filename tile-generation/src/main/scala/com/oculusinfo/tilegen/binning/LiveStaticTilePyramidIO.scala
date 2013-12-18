267c56162c042a295ce5b5312ff0793e5438b911
Nathan Kronenfeld <nkronenfeld@oculusinfo.com>
2013-12-18 17:27:23 -0500

    Add bounds object to live pyramid IO.  Calculate a bounds functino and a spreader function (a function to spread an input point across valid tiles).  Support spreader function in the binner

--- tile-generation/src/main/scala/com/oculusinfo/tilegen/binning/LiveStaticTilePyramidIO.scala
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/binning/LiveStaticTilePyramidIO.scala
@@ -22,16 +22,19 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
+
+
 package com.oculusinfo.tilegen.binning
 
 
 
-import java.lang.Iterable
-import java.util.{List => JavaList}
 import java.io.InputStream
 import java.io.IOException
+import java.lang.{Iterable => JavaIterable}
+import java.util.{List => JavaList}
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable.MutableList
 import scala.collection.mutable.{Map => MutableMap}
 
@@ -43,18 +46,24 @@ import com.oculusinfo.binning.TilePyramid
 import com.oculusinfo.binning.io.PyramidIO
 import com.oculusinfo.binning.io.TileSerializer
 
+import com.oculusinfo.tilegen.util.Rectangle
 import com.oculusinfo.tilegen.spark.GeneralSparkConnector
 import com.oculusinfo.tilegen.tiling.TileMetaData
 
 
 
+
+/**
+ * This class reads and caches a data set for live queries of its tiles
+ */
 class LiveStaticTilePyramidIO (master: String,
                                sparkHome: String,
                                user: Option[String]) extends PyramidIO {
   // We need to generate metadata for each 
   private val sc = new GeneralSparkConnector(master, sparkHome, user).getSparkContext("Live Static Tile Generation")
-  private val metaData = MutableMap[String, TileMetaData]()
-  private val data = MutableMap[String, RDD[(Double, Double, Double)]]()
+  private val metaDatas = MutableMap[String, TileMetaData]()
+  private val pyramids = MutableMap[String, TilePyramid]()
+  private val datas = MutableMap[String, RDD[(Double, Double, Double)]]()
 
 
   def initializeForWrite (pyramidId: String): Unit = {
@@ -63,7 +72,7 @@ class LiveStaticTilePyramidIO (master: String,
   def writeTiles[T] (pyramidId: String,
                      tilePyramid: TilePyramid,
                      serializer: TileSerializer[T],
-                     data: Iterable[TileData[T]]): Unit =
+                     data: JavaIterable[TileData[T]]): Unit =
     throw new IOException("Can't write raw data")
 
   def writeMetaData (pyramidId: String,
@@ -80,35 +89,95 @@ class LiveStaticTilePyramidIO (master: String,
                          tileSize: Int,
                          minLevel: Int,
                          maxLevel: Int): Unit = {
-    if (!data.contains(pyramidId)) {
-      data(pyramidId) = sc.textFile(pyramidId).map(line => {
+    if (!datas.contains(pyramidId)) {
+      datas(pyramidId) = sc.textFile(pyramidId).map(line => {
         val fields = line.split(',')
 
         (fields(0).toDouble, fields(1).toDouble, fields(2).toDouble)
       })
-      data(pyramidId).cache()
+      datas(pyramidId).cache()
     }
-    if (!metaData.contains(pyramidId)) {
-      metaData(pyramidId) = new TileMetaData(pyramidId,
+    if (!metaDatas.contains(pyramidId)) {
+      val fullBounds = tilePyramid.getTileBounds(new TileIndex(0, 0, 0, tileSize, tileSize))
+      metaDatas(pyramidId) = new TileMetaData(pyramidId,
                                              "Live static tile level",
                                              tileSize,
                                              tilePyramid.getTileScheme(),
                                              tilePyramid.getProjection(),
                                              0,
-                                             Int.MaxValue,
-                                             tilePyramid.getTileBounds(new TileIndex(0, 0, 0, tileSize, tileSize)),
+                                             scala.Int.MaxValue,
+                                             fullBounds,
                                              MutableList[(Int, String)](),
                                              MutableList[(Int, String)]())
     }
+    pyramids(pyramidId) = tilePyramid
+  }
+
+  /*
+   * Convert a set of tiles to testable bounds.
+   *
+   * The returned bounds are in two potential forms, paired into a 2-tuble.
+   *
+   * The first element is the bounds, in tile indices.
+   */
+  private def tilesToBounds (pyramid: TilePyramid,
+                             tiles: Iterable[TileIndex]): Bounds = {
+    var mutableRows = MutableList[Bounds]()
+    val bounds = tiles.map(tile => 
+      ((tile.getX, tile.getY()),
+       new Bounds(tile.getLevel(),
+                  new Rectangle[Int](tile.getX(), tile.getX(), tile.getY(), tile.getY()),
+                  None))
+    ).toSeq.sortBy(_._1).map(_._2).foreach(bounds => {
+      if (mutableRows.isEmpty) {
+        mutableRows += bounds
+      } else {
+        val last = mutableRows.last
+        val combination = last union bounds
+        if (combination.isEmpty) {
+          mutableRows += bounds
+        } else {
+          mutableRows(mutableRows.size-1) = combination.get
+        }
+      }
+    })
+
+    mutableRows.foldRight(None: Option[Bounds])((bounds, rest) =>
+      Some(new Bounds(bounds.level, bounds.indexBounds, rest))
+    ).map(_.reduce).getOrElse(None).get
   }
 
   def readTiles[T] (pyramidId: String,
                     serializer: TileSerializer[T],
-                    tiles: Iterable[TileIndex]): JavaList[TileData[T]] = {
-    data.get(pyramidId).map(datum => {
-      // Do processing here
+                    javaTiles: JavaIterable[TileIndex]): JavaList[TileData[T]] = {
+    val tiles = javaTiles.asScala
+    if (!datas.contains(pyramidId) ||
+        !metaDatas.contains(pyramidId) ||
+        !pyramids.contains(pyramidId) ||
+        !tiles.isEmpty) {
       null
-    }).getOrElse(null)
+    } else {
+      val metaData = metaDatas(pyramidId)
+      val pyramid = pyramids(pyramidId)
+      val data = datas(pyramidId)
+
+      val bounds = tilesToBounds(pyramid, tiles)
+
+      val boundsTest = bounds.getSerializableContainmentTest(pyramid)
+
+      // TODO:
+      //   * Modify RDDBinner to allow direct calling with already-set-up RDD
+      //     DONE (actually, already existed, but was private)
+      //   * Add a method in RDDBinner that allows the caller to specify individual 
+      //     tiles, rather than just levels
+      //     DONE
+      //     * Keep the overall bounds test as a quick first pass
+      //   * Add to Bounds a way of communicating to RDDBinner the tiles and bounds needed
+      //   * Call RDDBinner with the data set, filtered by overall bounds, and with the 
+      //     info needed to transform points to specific tiles
+
+      null
+    }
   }
 
   def getTileStream (pyramidId: String, tile: TileIndex): InputStream = {
@@ -116,6 +185,6 @@ class LiveStaticTilePyramidIO (master: String,
   }
 
   def readMetaData (pyramidId: String): String =
-    metaData.get(pyramidId).map(_.toString).getOrElse(null)
+    metaDatas.get(pyramidId).map(_.toString).getOrElse(null)
 
 }
---
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/util/Rectangle.scala
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2013 Oculus Info Inc.
+ * http://www.oculusinfo.com/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+package com.oculusinfo.tilegen.util
+
+
+
+import java.awt.geom.Rectangle2D
+
+
+
+/**
+ * A generic rectangle class that works on any value with an implicit ordering
+ */
+class Rectangle [T: Ordering] (val minX: T, val maxX: T, val minY: T, val maxY: T)
+extends Serializable {
+  private val ordering = implicitly[Ordering[T]]
+  lazy val xRange = (minX, maxX)
+  lazy val yRange = (minY, maxY)
+
+  /**
+   * Get the smallest rectangle fully containing both this and that
+   */
+  def union (that: Rectangle[T]): Rectangle[T] =
+    new Rectangle[T](ordering.min(this.minX, that.minX),
+		     ordering.max(this.maxX, that.maxX),
+		     ordering.min(this.minY, that.minY),
+		     ordering.max(this.maxY, that.maxY))
+
+  /**
+   * Determine if this rectangle contains the point specefied
+   */
+  def contains (x: T, y: T): Boolean =
+    (ordering.lteq(minX, x) && ordering.lteq(x, maxX) &&
+     ordering.lteq(minY, y) && ordering.lteq(y, maxY))
+
+  /**
+   * Determine if this rectangle contains the rectangle specified
+   */
+  def contains (that: Rectangle[T]): Boolean =
+    (ordering.lteq(this.minX, that.minX) && ordering.lteq(that.maxX, this.maxX) &&
+     ordering.lteq(this.minY, that.minY) && ordering.lteq(that.maxY, this.maxY))
+
+
+
+  // //////////////////////////////////////////////////////////////////////////
+  // Section: Any overrides
+  //
+  override def hashCode =
+    41*minX.hashCode + 43*maxX.hashCode + 47*minY.hashCode + 53*maxY.hashCode
+
+  override def equals (other: Any) = other match {
+    case that: Rectangle[_] =>
+      (this.minX == that.minX &&
+       this.maxX == that.maxX &&
+       this.minY == that.minY &&
+       this.maxY == that.maxY)
+    case _ => false
+  }
+
+  override def toString: String = "["+minX+", "+minY+" => "+maxX+", "+maxY+"]"
+}
+
+object Rectangle {
+  /**
+   * Convert a java rectangle into a Rectangle[Double]
+   */
+  def fromJava (source: Rectangle2D): Rectangle[Double] =
+    new Rectangle[Double](source.getMinX(), source.getMaxX(),
+			  source.getMinY(), source.getMaxY())
+}
+
