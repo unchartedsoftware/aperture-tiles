9b177ee66b0895dd52aab52265c0a2a66135e5bf
Nathan Kronenfeld <nkronenfeld@oculusinfo.com>
2014-01-20 20:37:21 -0500

    Working summation, min, and max functions for twitter demo records

--- binning-utilities/src/main/java/com/oculusinfo/binning/demo/TwitterDemoRecord.java
+++ binning-utilities/src/main/java/com/oculusinfo/binning/demo/TwitterDemoRecord.java
@@ -25,7 +25,10 @@ package com.oculusinfo.binning.demo;
 
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import com.oculusinfo.binning.io.Pair;
 
@@ -46,10 +49,10 @@ public class TwitterDemoRecord {
 
 
     public TwitterDemoRecord (String tag,
-                              Integer count, List<Integer> countBins,
-                              Integer positive, List<Integer> positiveBins,
-                              Integer neutral, List<Integer> neutralBins,
-                              Integer negative, List<Integer> negativeBins,
+                              int count, List<Integer> countBins,
+                              int positive, List<Integer> positiveBins,
+                              int neutral, List<Integer> neutralBins,
+                              int negative, List<Integer> negativeBins,
                               List<Pair<String, Long>> recent) {
         _tag = tag;
         _count = count;
@@ -172,5 +175,189 @@ public class TwitterDemoRecord {
                 "neutral: "  + _neutral  + ", neutralList: ["  + mkString(_neutralBins,  ", ") + "], " +
                 "negative: " + _negative + ", negativeList: [" + mkString(_negativeBins, ", ") + "], " +
                 "recent: [" + mkString(_recentTweets, ", ")+"]}");
+    }
+
+    private static void addInPlace (List<Integer> accumulatedSum, List<Integer> newAddend) {
+        for (int i=0; i<newAddend.size(); ++i) {
+            if (i >= accumulatedSum.size()) {
+                accumulatedSum.add(newAddend.get(i));
+            } else {
+                accumulatedSum.set(i, accumulatedSum.get(i) + newAddend.get(i));
+            }
+        }
+    }
+
+    private static void addRecentTweetInPlace (LinkedList<Pair<String, Long>> accumulatedTweets,
+                                               Pair<String, Long> newTweet) {
+        ListIterator<Pair<String, Long>> i = accumulatedTweets.listIterator();
+        int size = 0;
+        while (true) {
+            if (i.hasNext()) {
+                Pair<String, Long> next = i.next();
+                ++size;
+
+                if (next.getSecond() <= newTweet.getSecond()) {
+                    // Insert the new tweet...
+                    i.previous();
+                    i.add(newTweet);
+                    ++size;
+                    i.next();
+
+                    // ... and trim the list to 10 elements
+                    while (i.hasNext() && size < 10) {
+                        i.next();
+                        ++size;
+                    }
+                    while (i.hasNext()) {
+                        i.next();
+                        i.remove();
+                    }
+                    
+                    return;
+                }
+            } else {
+                i.add(newTweet);
+                return;
+            }
+        }
+    }
+
+    private static void addRecentTweetsInPlace (LinkedList<Pair<String, Long>> accumulatedTweets,
+                                                List<Pair<String, Long>> newTweets) {
+        for (Pair<String, Long> newTweet: newTweets) {
+            addRecentTweetInPlace(accumulatedTweets, newTweet);
+        }
+    }
+
+    /**
+     * Combine two records, summing all counts, and keeping the 10 most recent
+     * tweets. Tags must match for records to be combined.
+     */
+    public static TwitterDemoRecord add (TwitterDemoRecord... records) {
+        if (null == records || 0 == records.length)
+            return null;
+
+        String tag = records[0]._tag;
+        int count = records[0]._count;
+        int positive = records[0]._positive;
+        int neutral = records[0]._neutral;
+        int negative = records[0]._negative;
+        List<Integer> countBins = new ArrayList<>(records[0]._countBins);
+        List<Integer> positiveBins = new ArrayList<>(records[0]._positiveBins);
+        List<Integer> neutralBins = new ArrayList<>(records[0]._neutralBins);
+        List<Integer> negativeBins = new ArrayList<>(records[0]._negativeBins);
+        LinkedList<Pair<String, Long>> recentTweets = new LinkedList<>(records[0]._recentTweets);
+
+        for (int i=1; i<records.length; ++i) {
+            if (!objectsEqual(tag,  records[i]._tag))
+                throw new IllegalArgumentException("Cannot add twitter records for different tags");
+
+            count += records[i]._count;
+            positive += records[i]._positive;
+            neutral += records[i]._neutral;
+            negative += records[i]._negative;
+            addInPlace(countBins, records[i]._countBins);
+            addInPlace(positiveBins, records[i]._positiveBins);
+            addInPlace(neutralBins, records[i]._neutralBins);
+            addInPlace(negativeBins, records[i]._negativeBins);
+            addRecentTweetsInPlace(recentTweets, records[i]._recentTweets);
+        }
+        return new TwitterDemoRecord(tag,
+                                     count, countBins,
+                                     positive, positiveBins,
+                                     neutral, neutralBins,
+                                     negative, negativeBins,
+                                     recentTweets);
+    }
+
+    
+
+    private static void minInPlace (List<Integer> accumulatedMin, List<Integer> newMin) {
+        for (int i=0; i < newMin.size(); ++i) {
+            if (i >= accumulatedMin.size()) {
+                accumulatedMin.add(newMin.get(i));
+            } else {
+                accumulatedMin.set(i, Math.min(accumulatedMin.get(i), newMin.get(i)));
+            }
+        }
+    }
+
+    /**
+     * Get minimums of all counts across some number of records.
+     * @param that
+     * @return
+     */
+    public static TwitterDemoRecord min (TwitterDemoRecord... records) {
+        if (null == records || 0 == records.length)
+            return null;
+
+        int minCount = Integer.MAX_VALUE;
+        int minPositive = Integer.MAX_VALUE;
+        int minNeutral = Integer.MAX_VALUE;
+        int minNegative = Integer.MAX_VALUE;
+        List<Integer> minCountBins = new ArrayList<>();
+        List<Integer> minPositiveBins = new ArrayList<>();
+        List<Integer> minNeutralBins = new ArrayList<>();
+        List<Integer> minNegativeBins = new ArrayList<>();
+        for (TwitterDemoRecord record: records) {
+            minCount = Math.min(minCount, record._count);
+            minPositive = Math.min(minPositive, record._positive);
+            minNeutral = Math.min(minNeutral, record._neutral);
+            minNegative = Math.min(minNegative, record._negative);
+            minInPlace(minCountBins, record._countBins);
+            minInPlace(minPositiveBins, record._positiveBins);
+            minInPlace(minNeutralBins, record._neutralBins);
+            minInPlace(minNegativeBins, record._negativeBins);
+        }
+        return new TwitterDemoRecord(null,
+                                     minCount, minCountBins,
+                                     minPositive, minPositiveBins,
+                                     minNeutral, minNeutralBins,
+                                     minNegative, minNegativeBins,
+                                     new ArrayList<Pair<String, Long>>());
+    }
+
+    
+
+    private static void maxInPlace (List<Integer> accumulatedMax, List<Integer> newMax) {
+        for (int i=0; i < newMax.size(); ++i) {
+            if (i >= accumulatedMax.size()) {
+                accumulatedMax.add(newMax.get(i));
+            } else {
+                accumulatedMax.set(i, Math.max(accumulatedMax.get(i), newMax.get(i)));
+            }
+        }
+    }
+
+    /**
+     * Get maximums of all counts across some number of records.
+     * @param that
+     * @return
+     */
+    public static TwitterDemoRecord max (TwitterDemoRecord... records) {
+        int maxCount = Integer.MIN_VALUE;
+        int maxPositive = Integer.MIN_VALUE;
+        int maxNeutral = Integer.MIN_VALUE;
+        int maxNegative = Integer.MIN_VALUE;
+        List<Integer> maxCountBins = new ArrayList<>();
+        List<Integer> maxPositiveBins = new ArrayList<>();
+        List<Integer> maxNeutralBins = new ArrayList<>();
+        List<Integer> maxNegativeBins = new ArrayList<>();
+        for (TwitterDemoRecord record: records) {
+            maxCount = Math.max(maxCount, record._count);
+            maxPositive = Math.max(maxPositive, record._positive);
+            maxNeutral = Math.max(maxNeutral, record._neutral);
+            maxNegative = Math.max(maxNegative, record._negative);
+            maxInPlace(maxCountBins, record._countBins);
+            maxInPlace(maxPositiveBins, record._positiveBins);
+            maxInPlace(maxNeutralBins, record._neutralBins);
+            maxInPlace(maxNegativeBins, record._negativeBins);
+        }
+        return new TwitterDemoRecord(null,
+                                     maxCount, maxCountBins,
+                                     maxPositive, maxPositiveBins,
+                                     maxNeutral, maxNeutralBins,
+                                     maxNegative, maxNegativeBins,
+                                     new ArrayList<Pair<String, Long>>());
     }
 }
