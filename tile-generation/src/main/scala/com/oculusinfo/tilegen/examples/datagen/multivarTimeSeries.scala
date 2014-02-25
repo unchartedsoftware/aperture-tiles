a698bb33c39727d44949e65f06aff644dbc5f0f8
Chris Bethune <cbethune@oculusinfo.com>
2014-02-25 15:15:53 -0500

    refs #7232 - Initial checkin of multifractal generator.  It currently seems to be doing serial generation of the output textfile rather than parallel.

---
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/examples/datagen/multivarTimeSeries.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2014 Oculus Info Inc.
+ * http://www.oculusinfo.com/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oculusinfo.tilegen.examples.datagen
+
+import com.oculusinfo.tilegen.util.MissingArgumentException
+import com.oculusinfo.tilegen.util.ArgumentParser
+import java.util.GregorianCalendar
+import java.util.Calendar
+import java.util.Date
+import scala.collection.immutable.NumericRange
+import org.apache.velocity.runtime.directive.Foreach
+import scala.util.Random
+
+object MultivarTimeSeries {
+	// Source variable names
+	private val varNames = Array("Alpha", "Bravo", "Charlie", "Delta", "Echo", "Hotel", "Foxtrot", "Golf",
+			"Hotel", "India", "Juliet", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
+			"Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu")
+ 
+  /**
+   * Breaks the input time range up into sub-ranges and returns them as a list.  If the step in the time range is
+   * not equally divided by the step, the range will be truncated.
+   */
+  private def computePartitionRanges(partitions: Int, timeRange: NumericRange[Long]) = {
+    val partitionExtra = (timeRange.end - timeRange.start) % timeRange.step
+    val aliasedRange = (timeRange.start to timeRange.end - partitionExtra by timeRange.step)
+    val partitionSize = (aliasedRange.end - aliasedRange.start) / aliasedRange.step / partitions * aliasedRange.step
+
+    def computePartitionRange(partitionRange: NumericRange[Long], partitionNumber: Int): Seq[NumericRange[Long]] = {
+      val newRangeList = List(partitionRange.start to partitionRange.end by partitionRange.step)
+      if (partitionNumber == partitions - 1) {
+        newRangeList ++: computePartitionRange(
+            partitionRange.end + aliasedRange.step to aliasedRange.end by aliasedRange.step, partitionNumber + 1)
+      } else if (partitionNumber < partitions - 1) {
+        newRangeList ++: computePartitionRange(
+            partitionRange.end + aliasedRange.step to partitionRange.end + partitionSize + aliasedRange.step by aliasedRange.step, partitionNumber + 1)
+      } else {
+        newRangeList
+      }
+    }
+    computePartitionRange(aliasedRange.start to aliasedRange.start + partitionSize by aliasedRange.step, 1)
+  }
+     
+  /**
+   * Creates a sequence of names based on a sequence source values.  Names are formatted
+   * name-#.
+   */
+  private def generateNames(numNames: Int, names: IndexedSeq[_]) = {
+    for (i <- 0 until numNames) yield names(i % names.length) + "-" + i / names.length
+  }
+
+  /**
+   * Generates a time series where each timestamp has a corresponding set of variable values generated
+   * using perlin noise.
+   */
+  private def generateVarData(timePeriod: NumericRange[Long], variables: Seq[String], h: Double, octaves: Int, lacunarity: Double) = {
+		val results = List(variables.mkString("# time,", ",", ""))
+		val timeRange = timePeriod.end - timePeriod.start
+		val offsets = Array.fill(variables.length)(Random.nextDouble);
+		val varData = timePeriod map { timeStamp =>
+		  val noiseVals = for (i <- 0 until variables.length) yield {
+		    Multifractal.fBm( ((timeStamp.toDouble - timePeriod.start) / timeRange) + 0.1, offsets(i), 0.0, h, octaves, lacunarity) 
+		  }
+		  noiseVals.mkString(timeStamp + ",", ",", "")
+		}
+		results ++ varData
+  }
+  
+  /**
+   * Spark job main
+   */
+  def main(args: Array[String]): Unit = {    
+    val argParser = new ArgumentParser(args)
+    try {
+      
+    	val fileName = argParser.getStringArgument("output", "Name for generated output files", Some("multivar_data"))
+
+    	val numPoints = argParser.getIntArgument("timestamps", "Number of timestamps to generate", Some(1000))
+      val partitions = argParser.getIntArgument("partitions", "Number of partitions to split the data into when generating", Some(4))
+  
+      val numVariables = argParser.getIntArgument("variables", "Number of variables for each timestamp", Some(10))
+      val variables = generateNames(numVariables, varNames)
+
+      val octaves = argParser.getIntArgument("octaves", "Number of frequences to use in random signal", Some(6))
+      val h = argParser.getDoubleArgument("h", "Noise increment value", Some(0.1))
+      val lacunarity = argParser.getDoubleArgument("lacunarity", "Gap between frequences in random signal", Some(2.0))
+      
+      val startTime = new Date().getTime()
+      val cal = new GregorianCalendar()
+      cal.add(Calendar.DAY_OF_MONTH, 30)
+      val endTime = cal.getTime().getTime()
+      val timeRange = (startTime to endTime by (endTime - startTime) / numPoints)
+
+      val jobName = "Multi-variable Timeseries Generator"
+      val sc = argParser.getSparkConnector().getSparkContext(jobName)
+    	
+      val parallelCollection = sc.parallelize(computePartitionRanges(partitions, timeRange))
+      
+      // Run spark parallel job against each time range and save the result to a text file
+      parallelCollection flatMap { generateVarData(_, variables, h, octaves, lacunarity) } saveAsTextFile(fileName)         
+      sc.stop()
+    } catch {
+      case e: MissingArgumentException => {
+        println("Argument exception: " + e.getMessage())
+      }
+    }
+  }
+}
