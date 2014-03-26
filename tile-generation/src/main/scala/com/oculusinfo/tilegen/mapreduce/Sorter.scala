5a18fa5c6345128f69ed33ca3964f6ff1990e25e
Nathan Kronenfeld <nkronenfeld@oculusinfo.com>
2014-03-26 01:57:45 -0400

    Working map/reduce sorter class

--- tile-generation/src/main/scala/com/oculusinfo/tilegen/mapreduce/Sorter.scala
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/mapreduce/Sorter.scala
@@ -31,26 +31,33 @@ import java.lang.{Iterable => JavaIterable}
 
 import scala.collection.JavaConverters._
 
-import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.conf.Configured
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{Text, LongWritable}
 import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
 import org.apache.hadoop.mapreduce.lib.input.{FileInputFormat, TextInputFormat}
 import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, TextOutputFormat}
+import org.apache.hadoop.util.{Tool, ToolRunner}
 
 import com.oculusinfo.binning.PyramidComparator
 import com.oculusinfo.binning.TilePyramid
 import com.oculusinfo.binning.impl.AOITilePyramid
 
 
-class PyramidSortMapper extends Mapper[Text, Text, LongWritable, Text] {
+class PyramidSortKey extends LongWritable {
+	override def hashCode =
+		((get() >> 33) & 0x7fffffffl).toInt
+}
+
+class PyramidSortMapper extends Mapper[LongWritable, Text, PyramidSortKey, Text] {
 	val pyramid = new AOITilePyramid(-2.0, -2.0, 2.0, 2.0)
 	val comparator = new PyramidComparator(pyramid)
-	val outputKey: LongWritable = new LongWritable()
+	val outputKey: PyramidSortKey = new PyramidSortKey()
 	val outputValue: Text = new Text()
 
-	override def map (key: Text, value: Text,
-	                  context: Mapper[Text, Text, LongWritable, Text]#Context): Unit = {
+	override def map (key: LongWritable, value: Text,
+	                  context: Mapper[LongWritable, Text,
+	                                  PyramidSortKey, Text]#Context): Unit = {
 		val record = value.toString()
 		val fields = record.split('\t')
 		val (x, y) = (fields(0).toDouble, fields(1).toDouble)
@@ -60,10 +67,10 @@ class PyramidSortMapper extends Mapper[Text, Text, LongWritable, Text] {
 	}
 }
 
-class PyramidSortReducer extends Reducer[LongWritable, Text, Text, Text] {
+class PyramidSortReducer extends Reducer[PyramidSortKey, Text, Text, Text] {
 	val keyText: Text = new Text()
-	override def reduce (key: LongWritable, values: JavaIterable[Text],
-	                     context: Reducer[LongWritable, Text, Text, Text]#Context): Unit = {
+	override def reduce (key: PyramidSortKey, values: JavaIterable[Text],
+	                     context: Reducer[PyramidSortKey, Text, Text, Text]#Context): Unit = {
 		keyText.set(""+key.get())
 		values.asScala.foreach(text =>
 			context.write(text, keyText)
@@ -73,11 +80,13 @@ class PyramidSortReducer extends Reducer[LongWritable, Text, Text, Text] {
 
 
 
-object PyramidSorter {
-	def main (args: Array[String]): Unit = {
-		val conf = new Configuration()
-		val job = new Job(conf, "Pyramid Sorter")
+class PyramidSorter extends Configured with Tool {
+	override def run (args: Array[String]): Int = {
+		val job = Job.getInstance(getConf(), "Pyramid Sorter")
+
 		job.setJarByClass(this.getClass)
+        job.setMapOutputKeyClass(classOf[PyramidSortKey])
+        job.setMapOutputValueClass(classOf[Text])
 		job.setMapperClass(classOf[PyramidSortMapper])
 		job.setReducerClass(classOf[PyramidSortReducer])
 		job.setOutputKeyClass(classOf[Text])
@@ -85,10 +94,14 @@ object PyramidSorter {
 		FileInputFormat.addInputPath(job, new Path(args(0)))
 		FileOutputFormat.setOutputPath(job, new Path(args(1)))
 
-		val result = job.waitForCompletion(true)
-		if (result)
-			System.exit(0)
+		if (job.waitForCompletion(true))
+			return 0
 		else
-			System.exit(1)
+			return 1
+	}
+}
+object PyramidSorter {
+	def main (args: Array[String]): Unit = {
+		System.exit(ToolRunner.run(new PyramidSorter(), args))
 	}
 }
