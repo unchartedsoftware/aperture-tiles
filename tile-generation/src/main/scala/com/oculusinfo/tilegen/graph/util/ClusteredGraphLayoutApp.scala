adc129465eb2bb8ee4574c73b0222d78e2f1a228
dgiesbrecht <dgiesbrecht@oculusinfo.com>
2014-11-06 14:25:29 -0500

    Removed hard-coded references to SNAPSHOT version

--- tile-generation/src/main/scala/com/oculusinfo/tilegen/graph/util/ClusteredGraphLayoutApp.scala
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/graph/util/ClusteredGraphLayoutApp.scala
@@ -42,7 +42,7 @@ object ClusteredGraphLayoutApp {
 		argParser.debug
 
 		val jars =
-			Seq(new MavenReference("com.oculusinfo", "tile-generation", "0.4-SNAPSHOT")
+			Seq(new MavenReference("com.oculusinfo", "tile-generation", SparkConnector.getDefaultVersions("base"))
 			) union SparkConnector.getDefaultLibrariesFromMaven
 		val sc = argParser.getSparkConnector(jars).getSparkContext("Clustered Graph Layout") 
 		val sourceDir = argParser.getString("source", "The source directory where to find clustered graph data")
