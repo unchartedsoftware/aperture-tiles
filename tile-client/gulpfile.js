bf72713198e774ddff9f6482ce3ca57a2e200a85
kbirk <kbirk@oculusinfo.com>
2014-12-23 17:22:10 -0500

    refs #8703 - Added mocha, instabul, and lodash to package.json. Added gulp 'test' task to run all mocha tests inside /test. Added 'test' task to gradle.build.

--- tile-client/gulpfile.js
+++ tile-client/gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp'),
   	del = require('del'),
     concat = require('gulp-concat'),
-    csso = require('gulp-csso'),
+    csso = require('gulp-csso'),    
+    istanbul = require('gulp-istanbul'),
+    mocha = require('gulp-mocha'),
     jshint = require('gulp-jshint'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
     uglify = require('gulp-uglify');
-
 
 function bundle( b, output ) {
     return b.bundle()
@@ -60,6 +61,17 @@ gulp.task('lint', function() {
              .pipe( jshint.reporter('jshint-stylish') );
 });
 
+gulp.task('test', function() {
+    return gulp.src( [ 'src/js/**/*.js' ] )
+       .pipe( istanbul( { includeUntested: false } ) ) // Covering files
+       .on( 'finish', function () {
+            gulp.src( [ 'test/**/*.js' ] )
+                .pipe( mocha( { reporter: 'spec' } )
+                    .on( 'error', handleError) ) // print mocha error message
+                .pipe( istanbul.writeReports() ); // Creating the reports after tests runned
+        });
+});
+
 gulp.task('build-min-css', function () {
     return gulp.src( 'src/css/*.css' )
         .pipe( csso() )
@@ -81,21 +93,29 @@ gulp.task('build-js', [ 'lint' ], function() {
     return build( './src/js/api.js', 'tiles.js' );
 });
 
-gulp.task('build-rest-min-js', [ 'lint' ], function() {
-    return buildMin( './src/js/rest-api.js', 'tiles-rest.min.js' );
-});
+// gulp.task('build-rest-min-js', [ 'lint' ], function() {
+//     return buildMin( './src/js/rest-api.js', 'tiles-rest.min.js' );
+// });
 
-gulp.task('build-rest-js', [ 'lint' ], function() {
-    return build( './src/js/rest-api.js', 'tiles-rest.js' );
-});
+// gulp.task('build-rest-js', [ 'lint' ], function() {
+//     return build( './src/js/rest-api.js', 'tiles-rest.js' );
+// });
 
 gulp.task('build', [ 'clean' ], function() {
-    gulp.start( 'build-js' );
-    gulp.start( 'build-rest-js' );
+    gulp.start( 'build-js' );   
     gulp.start( 'build-css' );   
-    gulp.start( 'build-min-js' );;
-    gulp.start( 'build-rest-min-js' );
+    gulp.start( 'build-min-js' );
     gulp.start( 'build-min-css' );
+    //gulp.start( 'build-rest-min-js' );
+    //gulp.start( 'build-rest-js' );
+});
+
+gulp.task('debugjs', function() {
+    gulp.start( 'build-js' );
+});
+
+gulp.task('debugcss', function() {
+    gulp.start( 'build-css' );
 });
 
 gulp.task('default', [ 'build' ], function() {
