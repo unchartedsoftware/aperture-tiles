433019f4ca73a571ad81bdd0df05afb43090ba20
Nathan Kronenfeld <nkronenfeld@oculusinfo.com>
2015-01-30 02:43:19 -0500

    Fix indentation in SparseTileData

--- binning-utilities/src/main/java/com/oculusinfo/binning/SparseTileData.java
+++ binning-utilities/src/main/java/com/oculusinfo/binning/SparseTileData.java
@@ -41,169 +41,169 @@ import java.util.*;
  * @param <T> The type of data stored in the bins of this tile.
  */
 public class SparseTileData<T> extends TileDataMetadataImpl<T> implements TileData<T> {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 
 
-    private TileIndex                        _definition;
-    // We keep data as a double-map to avoid having to construct a BinIndex each query.
-    private Map<Integer, Map<Integer, T>>    _data;
-    private T                                _defaultValue;
+	private TileIndex                        _definition;
+	// We keep data as a double-map to avoid having to construct a BinIndex each query.
+	private Map<Integer, Map<Integer, T>>    _data;
+	private T                                _defaultValue;
 
-    // No-argument constructor, really just for use by Kryo, but we call it from
-    // the main constructor just to get rid of the warning.
-    private SparseTileData () {
-        super();
-    }
+	// No-argument constructor, really just for use by Kryo, but we call it from
+	// the main constructor just to get rid of the warning.
+	private SparseTileData () {
+		super();
+	}
 
-    /**
-     * Construct an empty sparse tile data object for a particular tile with no data.
-     *
-     * @param definition The index of the tile whose data is to be collected by this object.
-     */
-    public SparseTileData (TileIndex definition) {
-        this(definition, (T) null);
-    }
+	/**
+	 * Construct an empty sparse tile data object for a particular tile with no data.
+	 *
+	 * @param definition The index of the tile whose data is to be collected by this object.
+	 */
+	public SparseTileData (TileIndex definition) {
+		this(definition, (T) null);
+	}
 
-    /**
-     * Construct an empty, sparse tile for a particular tile index.
-     *
-     * @param definition The index of the tile whose data is to be
-     *            collected by this object.
-     * @param defaultValue The default value of each bin
-     */
-    public SparseTileData (TileIndex definition, T defaultValue) {
-        this();
-        _definition = definition;
-        _defaultValue = defaultValue;
-        _data = new HashMap<>();
-    }
+	/**
+	 * Construct an empty, sparse tile for a particular tile index.
+	 *
+	 * @param definition The index of the tile whose data is to be
+	 *            collected by this object.
+	 * @param defaultValue The default value of each bin
+	 */
+	public SparseTileData (TileIndex definition, T defaultValue) {
+		this();
+		_definition = definition;
+		_defaultValue = defaultValue;
+		_data = new HashMap<>();
+	}
 
-    /**
-     * Construct a tile for a particular tile index, with preset data. Note the passed-in preset data is used as is,
-     * not copied.
-     *
-     * @param definition
-     *            The index of the tile whose data is to be represented by this
-     *            object.
-     * @param tileData
-     *            The data for this tile.  The index of the outer map specifies the X coordinate of the bin; the inner
-     *            map, the Y coordinate.
-     */
-    public SparseTileData (TileIndex definition, Map<Integer, Map<Integer, T>> tileData, T defaultValue) {
-        super();
-        _definition = definition;
-        _defaultValue = defaultValue;
-        _data = tileData;
-    }
-
-
-
-    /** {@inheritDoc} */
-    @Override
-    public TileIndex getDefinition () {
-        return _definition;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void setBin(int x, int y, T value) {
-        if (x < 0 || x >= _definition.getXBins()) {
-            throw new IllegalArgumentException("Bin x index is outside of tile's valid bin range");
-        }
-        if (y < 0 || y >= _definition.getYBins()) {
-            throw new IllegalArgumentException("Bin y index is outside of tile's valid bin range");
-        }
-
-        if (!_data.containsKey(x)) _data.put(x, new HashMap<Integer, T>());
-        _data.get(x).put(y, value);
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public T getBin(int x, int y) {
-        if (x < 0 || x >= _definition.getXBins()) {
-            throw new IllegalArgumentException("Bin x index is outside of tile's valid bin range");
-        }
-        if (y < 0 || y >= _definition.getYBins()) {
-            throw new IllegalArgumentException("Bin y index is outside of tile's valid bin range");
-        }
-
-        if (_data.containsKey(x)) {
-            Map<Integer, T> xMap = _data.get(x);
-            if (xMap.containsKey(y)) {
-                return xMap.get(y);
-            }
-        }
-        return _defaultValue;
-    }
-
-    private static List<Integer> getKeys (Map<Integer, ?> map) {
-        List<Integer> vals = new ArrayList<Integer>(map.keySet());
-        Collections.sort(vals);
-        return vals;
-    }
-
-    /**
-     * Get an iterator over all our defined data, ignoring defaulted bins
-     */
-    public Iterator<Pair<BinIndex, T>> getData () {
-        return new DataIterator();
-    }
-
-    /**
-     * Get the default value for bins not defined in our sparse array.
-     */
-    public T getDefaultBinValue () {
-        return _defaultValue;
-    }
+	/**
+	 * Construct a tile for a particular tile index, with preset data. Note the passed-in preset data is used as is,
+	 * not copied.
+	 *
+	 * @param definition
+	 *            The index of the tile whose data is to be represented by this
+	 *            object.
+	 * @param tileData
+	 *            The data for this tile.  The index of the outer map specifies the X coordinate of the bin; the inner
+	 *            map, the Y coordinate.
+	 */
+	public SparseTileData (TileIndex definition, Map<Integer, Map<Integer, T>> tileData, T defaultValue) {
+		super();
+		_definition = definition;
+		_defaultValue = defaultValue;
+		_data = tileData;
+	}
 
 
 
-    private class DataIterator implements Iterator<Pair<BinIndex, T>> {
-        private Iterator<Integer> _xVals = getKeys(_data).iterator();
-        private int x;
-        private Iterator<Integer> _yVals = null;
+	/** {@inheritDoc} */
+	@Override
+	public TileIndex getDefinition () {
+		return _definition;
+	}
 
-        private boolean hasNextY () {
-            return (null != _yVals && _yVals.hasNext());
-        }
+	/** {@inheritDoc} */
+	@Override
+	public void setBin(int x, int y, T value) {
+		if (x < 0 || x >= _definition.getXBins()) {
+			throw new IllegalArgumentException("Bin x index is outside of tile's valid bin range");
+		}
+		if (y < 0 || y >= _definition.getYBins()) {
+			throw new IllegalArgumentException("Bin y index is outside of tile's valid bin range");
+		}
 
-        private boolean incrementX () {
-            if (!_xVals.hasNext()) return false;
+		if (!_data.containsKey(x)) _data.put(x, new HashMap<Integer, T>());
+		_data.get(x).put(y, value);
+	}
 
-            x = _xVals.next();
-            _yVals = getKeys(_data.get(x)).iterator();
-            return true;
-        }
+	/** {@inheritDoc} */
+	@Override
+	public T getBin(int x, int y) {
+		if (x < 0 || x >= _definition.getXBins()) {
+			throw new IllegalArgumentException("Bin x index is outside of tile's valid bin range");
+		}
+		if (y < 0 || y >= _definition.getYBins()) {
+			throw new IllegalArgumentException("Bin y index is outside of tile's valid bin range");
+		}
 
-        @Override
-        public boolean hasNext() {
-            // If we can increment Y, then we're done - we have another value
-            if (hasNextY()) return true;
+		if (_data.containsKey(x)) {
+			Map<Integer, T> xMap = _data.get(x);
+			if (xMap.containsKey(y)) {
+				return xMap.get(y);
+			}
+		}
+		return _defaultValue;
+	}
 
-            // If we can't increment X, then we're done - we don't have another value
-            if (!incrementX()) return false;
+	private static List<Integer> getKeys (Map<Integer, ?> map) {
+		List<Integer> vals = new ArrayList<Integer>(map.keySet());
+		Collections.sort(vals);
+		return vals;
+	}
 
-            // We may or may not be done, we have to check if there is a Y in the next X.
-            return hasNext();
-        }
+	/**
+	 * Get an iterator over all our defined data, ignoring defaulted bins
+	 */
+	public Iterator<Pair<BinIndex, T>> getData () {
+		return new DataIterator();
+	}
 
-        @Override
-        public Pair<BinIndex, T> next() {
-            if (hasNextY()) {
-                int y = _yVals.next();
-                return new Pair<BinIndex, T>(new BinIndex(x, y), getBin(x, y));
-            } else if (incrementX()) {
-                return next();
-            } else {
-                return null;
-            }
-        }
+	/**
+	 * Get the default value for bins not defined in our sparse array.
+	 */
+	public T getDefaultBinValue () {
+		return _defaultValue;
+	}
 
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException("Illegal to remove elements from SparseTileData.getData()");
-        }
-    }
+
+
+	private class DataIterator implements Iterator<Pair<BinIndex, T>> {
+		private Iterator<Integer> _xVals = getKeys(_data).iterator();
+		private int x;
+		private Iterator<Integer> _yVals = null;
+
+		private boolean hasNextY () {
+			return (null != _yVals && _yVals.hasNext());
+		}
+
+		private boolean incrementX () {
+			if (!_xVals.hasNext()) return false;
+
+			x = _xVals.next();
+			_yVals = getKeys(_data.get(x)).iterator();
+			return true;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// If we can increment Y, then we're done - we have another value
+			if (hasNextY()) return true;
+
+			// If we can't increment X, then we're done - we don't have another value
+			if (!incrementX()) return false;
+
+			// We may or may not be done, we have to check if there is a Y in the next X.
+			return hasNext();
+		}
+
+		@Override
+		public Pair<BinIndex, T> next() {
+			if (hasNextY()) {
+				int y = _yVals.next();
+				return new Pair<BinIndex, T>(new BinIndex(x, y), getBin(x, y));
+			} else if (incrementX()) {
+				return next();
+			} else {
+				return null;
+			}
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException("Illegal to remove elements from SparseTileData.getData()");
+		}
+	}
 }
