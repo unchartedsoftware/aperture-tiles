65d2dbd3c071af52d90efdde99a672ac1491a783
Chris Bethune <cbethune@oculusinfo.com>
2015-02-04 16:20:10 -0500

    Adds documentation for public definitions

--- tile-generation/src/main/scala/com/oculusinfo/tilegen/tiling/TileOperations.scala
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/tiling/TileOperations.scala
@@ -40,22 +40,26 @@ import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
  *
  */
 // TODO: Provide example of usage in object docs above
-// TODO: Document args for each operation
 // TODO: Add error checking to all operations and parsers
 object TileOperations {
   import com.oculusinfo.tilegen.datasets.SchemaTypeUtilities._
 
-import scala.collection.JavaConversions._
+  import scala.collection.JavaConversions._
 
   /**
-   * KeyValueArgumentSource implementation that simply passes the supplied map through.
+   * KeyValueArgumentSource implementation that passes the supplied map through.
+   *
+   * @param args Wrapped argument map.
    */
   case class KeyValuePassthrough(args: Map[String, String]) extends KeyValueArgumentSource {
     def properties = args.map(entry => entry._1.toLowerCase -> entry._2)
   }
 
   /**
-   * Registers this standard set of ops
+   * Registers a standard set of operations against a pipeline.
+   *
+   * @param tilePipeline Unique ID of the pipeline registered against.
+   * @return Updated tile pipeline object.
    */
   def registerOperations(tilePipeline: TilePipelines) = {
     // Register basic pipeline operations
@@ -71,7 +75,11 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse data path from args
+   * Parses arguments for the JSON load operation, and creates an instance
+   * of a pipeline operation with those arguments applied to it.
+   *
+   * Arguments:
+   *    ops.path - Valid HDFS path to data.
    */
   def parseLoadJsonDataOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -81,13 +89,24 @@ import scala.collection.JavaConversions._
 
   /**
    * Load data from a JSON file.  The schema is derived from the first json record.
+   *
+   * @param path Valid HDFS path to the data.
+   * @param data Not used.
+   * @return PipelineData with a schema RDD populated from the JSON file.
    */
   def loadJsonDataOp(path: String)(data: PipelineData): PipelineData = {
     PipelineData(data.sqlContext, data.sqlContext.jsonFile(path))
   }
 
   /**
-   * Parse data path from args
+   * Parses arguments for the CSV load operation, and creates an instance
+   * of a pipeline operation with those arguments applied to it.
+   *
+   * Arguments:
+   *    ops.path - Valid HDFS path to data.
+   *
+   * @see com.oculusinfo.tilegen.datasets.CSVReader for arguments passed into the CSV reader.
+   *
    */
   def parseLoadCsvDataOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -96,7 +115,14 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Load data from a CSV file.  The schema is derived from the first json record.
+   * Load data from a CSV file using CSVReader.
+   * The arguments map will be passed through to that object, so all arguments required
+   * for its configuration should be set in the map.
+   *
+   * @param path HDSF path to the data object.
+   * @param argumentSource Arguments to forward to the CSVReader.
+   * @param data Not used.
+   * @return PipelineData with a schema RDD populated from the CSV file.
    */
   def loadCsvDataOp(path: String, argumentSource: KeyValueArgumentSource)(data: PipelineData): PipelineData = {
     val reader = new CSVReader(data.sqlContext, path, argumentSource)
@@ -104,7 +130,14 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse start / end times from args.
+   * Parse start / end times from args for the date filter op, and creates an instance
+   * of a pipeline operation with those arguments applied to it.
+   *
+   * Arguments:
+   *    ops.column - Column spec denoting the time field in input data
+   *    ops.start - Start date string
+   *    ops.end - End date string
+   *    ops.format - Date format string (based on [[java.text.SimpleDateFormat]])
    */
   def parseDateFilterOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -116,7 +149,14 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Pipeline op to filter records to a specific date range
+   * Pipeline op to filter records to a specific date range.
+   *
+   * @param minDate Start date for the range, expressed in a format parsable by [[java.text.SimpleDateFormat]].
+   * @param maxDate End date for the range, expressed in a format parsable by [[java.text.SimpleDateFormat]].
+   * @param format Date parsing string, expressed according to [[java.text.SimpleDateFormat]].
+   * @param timeCol Column spec denoting name of time column in input schema RDD.
+   * @param input Input pipeline data to filter.
+   * @return Transformed pipeline data, where records outside the specified time range have been removed.
    */
   def dateFilterOp(minDate: String, maxDate: String, format: String, timeCol: String)(input: PipelineData) = {
     val formatter = new SimpleDateFormat(format)
@@ -131,14 +171,15 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parses args for cache data.  No arguments required.
+   * Parses args for cache data operation.  No arguments required.
    */
   def parseCacheDataOp(args: Map[String, String]) = {
     cacheDataOp()(_)
   }
 
   /**
-   * Pipeline op to cache data
+   * Pipeline op to cache data - this allows for subsequent stages in the pipeline to run against computed
+   * results, rather than the input data set.
    */
   def cacheDataOp()(input: PipelineData) = {
     input.srdd.registerTempTable("cached_table")
@@ -147,20 +188,35 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse args for integral n-dimensional range filter
+   * Parses args for an integral n-dimensional range filter and returns an instance of the filter
+   * with those arguments applied to it.  The dimensionality is consistent across the min, max and
+   * columns arguments.
+   *
+   * Arguments:
+   *  ops.min - sequence of min values, 1 for each dimension of the data
+   *  ops.max - sequence of max values, 1 for each dimension of the data
+   *  ops.exclude - boolean indicating whether values in the range are excluded or included.
+   *  ops.columns - sequence of column specs, 1 for each dimension of the data
    */
   def parseIntegralRangeFilterOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
     val min = argParser.getLongSeq("ops.min", "min value")
     val max = argParser.getLongSeq("ops.max", "max value")
     val exclude = argParser.getBoolean("ops.exclude", "exclude values in the range", Some(false))
-    val colSpecs = argParser.getStringSeq("ops.columns", "numeric column id")
+    val colSpecs = argParser.getStringSeq("ops.columns", "column ids")
     integralRangeFilterOp(min, max, colSpecs, exclude)(_)
   }
 
   /**
-  * A generalized n-dimensional range filter operation for integral types.
-  */
+   * A generalized n-dimensional range filter operation for integral types.
+   *
+   * @param min Sequence of min values, 1 for each dimension of the data
+   * @param max Sequence of max values, 1 for each dimension of the data
+   * @param colSpecs Sequence of column specs, 1 for each dimension of the data
+   * @param exclude Boolean indicating whether values in the range are excluded or included.
+   * @param input Pipeline data to apply filter to
+   * @return Transformed pipeline data, where records inside/outside the specified time range have been removed.
+   */
   def integralRangeFilterOp(min: Seq[Long], max: Seq[Long], colSpecs: Seq[String], exclude: Boolean = false)(input: PipelineData) = {
     val extractors = colSpecs.map(cs => calculateExtractor(cs, input.srdd.schema))
     val result = input.srdd.filter { row =>
@@ -172,7 +228,15 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse args for n-dimensional fractional range filter
+   * Parses args for a fractional n-dimensional range filter and returns an instance of the filter
+   * with those arguments applied to it.  The dimensionality is consistent across the min, max and
+   * columns arguments.
+   *
+   * Arguments:
+   *  ops.min - sequence of min values, 1 for each dimension of the data
+   *  ops.max - sequence of max values, 1 for each dimension of the data
+   *  ops.exclude - boolean indicating whether values in the range are excluded or included.
+   *  ops.columns - sequence of column specs, 1 for each dimension of the data
    */
   def parseFractionalRangeFilterOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -184,7 +248,14 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * A generalized n-dimensional range filter operation for fractional types
+   * A generalized n-dimensional range filter operation for fractional types.
+   *
+   * @param min Sequence of min values, 1 for each dimension of the data
+   * @param max Sequence of max values, 1 for each dimension of the data
+   * @param colSpecs Sequence of column specs, 1 for each dimension of the data
+   * @param exclude Boolean indicating whether values in the range are excluded or included.
+   * @param input Pipeline data to apply filter to
+   * @return Transformed pipeline data, where records inside/outside the specified time range have been removed.
    */
   def fractionalRangeFilterOp(min: Seq[Double], max: Seq[Double], colSpecs: Seq[String], exclude: Boolean = false)(input: PipelineData) = {
     val extractors = colSpecs.map(cs => calculateExtractor(cs, input.srdd.schema))
@@ -197,7 +268,13 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse args for regex filter operation
+   * Parse args for regex filter operation and returns an instance of the operation with those
+   * arguments applied.
+   *
+   * Arguments:
+   *  ops.regex - Regex string to use as filter
+   *  ops.column - Column spec denoting the column to test the regex against.
+   *  ops.exclude - Boolean indicating whether to exclude or include values that match
    */
   def parseRegexFilterOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -208,7 +285,13 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * A regex filter operation
+   * A regex filter operation.
+   *
+   * @param regexStr Regex string to use as filter
+   * @param colSpec Column spec denoting the column to test the regex against.
+   * @param exclude Boolean indicating whether to exclude or include values that match.
+   * @param input Pipeline data to apply the filter to.
+   * @return Transformed pipeline data, where records matching the regex are included or excluded.
    */
   def regexFilterOp(regexStr: String, colSpec: String, exclude: Boolean = false)(input: PipelineData) = {
     val regex = regexStr.r
@@ -223,7 +306,11 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse operation to create a new SchemaRDD from selected columns
+   * Parse operation args for operation that creates a new schema RDD from selected columns, and return
+   * an instance of that operation with those arguments applied.
+   *
+   * Arguments:
+   *  ops.columns - Sequence of column specs denoting the columns to select.
    */
   def parseColumnSelectOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
@@ -232,7 +319,11 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Operation to create a new SchemaRDD from selected columns
+   * Operation to create a new schema RDD from selected columns.
+   *
+   * @param colSpecs Sequence of column specs denoting the columns to select.
+   * @param input Pipeline data to select columns from.
+   * @return Pipeline data containing a schema RDD with only the selected columns.
    */
   def columnSelectOp(colSpecs: Seq[String])(input: PipelineData) = {
     val colExprs = colSpecs.map(UnresolvedAttribute(_))
@@ -241,16 +332,46 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * Parse args for basic heatmap tile generator.
+   * Parse args for basic heatmap tile generator operation that writes to HDFS, and return an instance of that
+   * operation with those arguments applied.
+   *
+   * Arguments:
+   *    hbase.zookeeper.quorum - Zookeeper quorum addresses specified as a comma separated list.
+   *    hbase.zookeeper.port - Zookeeper port.
+   *    hbase.master - HBase master address.
+   *    ops.xColumn - Colspec denoting data x column
+   *    ops.yColumn - Colspec denoting data y column
+   *
+   *    Tiling task consumes a TilingTaskParameters object that
+   *    is populated via an argument map.  The argument map passed to this function will be used for that
+   *    purpose, so all arguments required by that object should be set.
    */
   def parseHbaseHeatmapOp(args: Map[String, String]) = {
     val argParser = KeyValuePassthrough(args)
     val zookeeperQuorum = argParser.getString("hbase.zookeeper.quorum", "Zookeeper quorum addresses")
-    val zookeeperPort = argParser.getString("hbase.zookeeper.port", "Zookeeper quorum addresses")
-    val hbaseMaster = argParser.getString("hbase.master", "Zookeeper quorum addresses")
+    val zookeeperPort = argParser.getString("hbase.zookeeper.port", "Zookeeper port")
+    val hbaseMaster = argParser.getString("hbase.master", "HBase master address")
     val heatmapParams = parseHeatMapOpImpl(args, argParser)
 
     hbaseHeatMapOp(heatmapParams._1, heatmapParams._2, heatmapParams._3, zookeeperQuorum, zookeeperPort, hbaseMaster)(_)
+  }
+
+  /**
+   * Parse args for basic heatmap tile generator operation that writes to the local file system, and return an
+   * instance of that operation with those arguments applied.
+   *
+   * Arguments:
+   *    ops.xColumn - Colspec denoting data x column
+   *    ops.yColumn - Colspec denoting data y column
+   *
+   *    Tiling task consumes a TilingTaskParameters object that
+   *    is populated via an argument map.  The argument map passed to this function will be used for that
+   *    purpose, so all arguments required by that object should be set.
+   */
+  def parseFileHeatmapOp(args: Map[String, String]) = {
+    val argParser = KeyValuePassthrough(args)
+    val heatmapParams = parseHeatMapOpImpl(args, argParser)
+    fileHeatMapOp(heatmapParams._1, heatmapParams._2, heatmapParams._3)(_)
   }
 
   private def parseHeatMapOpImpl(args: Map[String, String], argParser: KeyValueArgumentSource) = {
@@ -265,38 +386,48 @@ import scala.collection.JavaConversions._
   }
 
   /**
-   * A basic heatmap tile generator that writes output to HDFS.
+   * A basic heatmap tile generator that writes output to HDFS. Uses
+   * TilingTaskParameters to manage tiling task arguments; the arguments map
+   * will be passed through to that object, so all arguments required for its configuration should be set in the map.
+   *
+   * @param xColSpec Colspec denoting data x column
+   * @param yColSpec Colspec denoting data y column
+   * @param tilingParams Parameters to forward to the tiling task.
+   * @param zookeeperQuorum Zookeeper quorum addresses specified as a comma separated list.
+   * @param zookeeperPort Zookeeper port.
+   * @param hbaseMaster HBase master address.
+   * @param input Pipeline data to tile.
+   * @return Unmodified input data.
    */
   def hbaseHeatMapOp(xColSpec: String,
                 yColSpec: String,
-                tilingParms: TilingTaskParameters,
+                tilingParams: TilingTaskParameters,
                 zookeeperQuorum: String,
                 zookeeperPort: String,
                 hbaseMaster: String)
                (input: PipelineData) = {
 
     val tileIO = new HBaseTileIO(zookeeperQuorum, zookeeperPort, hbaseMaster)
-    heatMapOpImpl(xColSpec, yColSpec, tilingParms, tileIO)(input)
+    heatMapOpImpl(xColSpec, yColSpec, tilingParams, tileIO)(input)
   }
 
   /**
-   * Parse args for basic heatmap tile generator.
-   */
-  def parseFileHeatmapOp(args: Map[String, String]) = {
-    val argParser = KeyValuePassthrough(args)
-    val heatmapParams = parseHeatMapOpImpl(args, argParser)
-    fileHeatMapOp(heatmapParams._1, heatmapParams._2, heatmapParams._3)(_)
-  }
-
-  /**
-   * A basic heatmap tile generator that writes output to the local file system.
+   * A basic heatmap tile generator that writes output to the local file system. Uses
+   * TilingTaskParameters to manage tiling task arguments; the arguments map
+   * will be passed through to that object, so all arguments required for its configuration should be set in the map.
+   *
+   * @param xColSpec Colspec denoting data x column
+   * @param yColSpec Colspec denoting data y column
+   * @param tilingParams Parameters to forward to the tiling task.
+   * @param input Pipeline data to tile.
+   * @return Unmodified input data.
    */
   def fileHeatMapOp(xColSpec: String,
                 yColSpec: String,
-                tilingParms: TilingTaskParameters)
+                tilingParams: TilingTaskParameters)
                (input: PipelineData) = {
     val tileIO = new LocalTileIO("avro")
-    heatMapOpImpl(xColSpec, yColSpec, tilingParms, tileIO)(input)
+    heatMapOpImpl(xColSpec, yColSpec, tilingParams, tileIO)(input)
   }
 
   private def heatMapOpImpl(xColSpec: String,
