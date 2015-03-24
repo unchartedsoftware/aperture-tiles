b6ce7f0eb1baab93c60a285cd8c2a3e757b45f46
Tom Lachapelle <tlachapelle@oculusinfo.com>
2015-03-24 14:34:30 -0400

    Crisis-46 - Time-based tile rendering - added a new time based filter transformer in conjuction with the DoubleListHeatMapImageRenderer in order to filter time ranges(buckets) used to determine heatmap values.  Updated DoubleListHeatMapImageRenderer to use Number instead of Double when processing tiles as well as taking into account null values in the tileData.  Null values in the tileData was necessary as the filter only deals with the templated value within the data and cannot just be Zeroed out.

---
+++ tile-rendering/src/main/java/com/oculusinfo/tile/rendering/transformations/tile/FilterByTimeTileTransformer.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2015 Uncharted Software. 
+ * http://www.oculusinfo.com/
+ * 
+ * Released under the MIT License.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oculusinfo.tile.rendering.transformations.tile;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.oculusinfo.binning.TileData;
+import com.oculusinfo.binning.TileIndex;
+import com.oculusinfo.binning.impl.DenseTileData;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/** 
+ * 	This transformer will take in JSON or tileData object representing bins of a double array  
+ * 		tile and will filter out all counts in the bins representing the time buckets indicated
+ * 		in the configuration.  The double arrays passed back will be in the order that they are 
+ * 		sequenced in the JSON/Tile array passed in.
+ * 
+ */
+
+public class FilterByTimeTileTransformer<T> implements TileTransformer<List<T>> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FilterByTimeTileTransformer.class);
+	
+	private int _startTimeBucket = 0;
+	private int _endTimeBucket = 0;
+	private int _totalTimeBuckets = 0;
+	
+	public FilterByTimeTileTransformer(JSONObject arguments){
+		try {
+			if (arguments != null) {
+				// get the start and end time range
+				_startTimeBucket = arguments.getInt("startTimeBucket");
+				_endTimeBucket = arguments.getInt("endTimeBucket");
+				_totalTimeBuckets = arguments.getInt("totalTimeBuckets");
+			}
+		}
+		catch (JSONException e) {
+			LOGGER.warn("Exception getting arguments for filter by time tile transformer", e);
+		}
+	}
+
+	
+	@Override
+	public JSONObject transform (JSONObject inputJSON) throws JSONException {
+		return inputJSON; // not implemented 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.oculusinfo.tile.rendering.transformations.tile.TileTransformer#transform(com.oculusinfo.binning.TileData)
+	 * 
+	 * Note: This transformer explicitly transforms all tiles into a dense tile format.  If a sparse tile is 
+	 * 			passed in, the values not explicitly represented will be set to null.
+	 */
+    @Override
+    public TileData<List<T>> transform (TileData<List<T>> inputData) throws Exception {
+
+    	TileData<List<T>> resultTile = null;
+        
+        if ( _startTimeBucket < 0 || _startTimeBucket > _endTimeBucket || _endTimeBucket > _totalTimeBuckets ) { 
+			throw new IllegalArgumentException("Filter by time transformer arguments are invalid.  start time bucket: " + _startTimeBucket + ", end time bucket: " + _endTimeBucket);
+        }      
+
+        int xBins = inputData.getDefinition().getXBins();
+        int yBins = inputData.getDefinition().getYBins();
+        
+        TileIndex index = inputData.getDefinition();
+		List<List<T>> transformedData = new ArrayList<>(index.getXBins()*index.getYBins());
+
+        for(int ty = 0; ty < yBins; ty++){
+            for(int tx = 0; tx < xBins; tx++){
+                List<T> binContents = inputData.getBin(tx, ty);
+                int binSize = binContents.size();
+                
+                // make sure we have a full array to add into the tile for dense tile creation
+                List<T> transformedBin = new ArrayList<>(_totalTimeBuckets);
+                for(int i = 0; i < binSize; i++) {
+                    if ( i >= _startTimeBucket && i <= _endTimeBucket ) {
+                    	transformedBin.add(i, binContents.get(i));
+                    } else {
+                    	transformedBin.add(i, null);
+                    }
+                }
+                transformedData.add(transformedBin);
+            }
+        }
+
+        resultTile = new DenseTileData<>(inputData.getDefinition(), transformedData);
+        
+        // add in metadata to the tile
+        Collection<String> keys = inputData.getMetaDataProperties();
+		if (null != keys && !keys.isEmpty()) {
+			for (String key: keys) {
+			String value = inputData.getMetaData(key);
+			if (null != value)
+				resultTile.setMetaData(key, value);
+			}
+		}
+
+        return resultTile;
+    }
+
+}
