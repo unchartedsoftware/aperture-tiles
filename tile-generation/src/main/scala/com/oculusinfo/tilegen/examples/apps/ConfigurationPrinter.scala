09bca94b4fc656b6bcd2fe4de67ded1168462fb5
U-OCULUS\nkronenfeld <nkronenfeld@8570w-pc.oculus.local>
2015-05-21 23:14:20 -0400

    Simple app to print out the spark configuration, so I can copy it in on-demand processes

---
+++ tile-generation/src/main/scala/com/oculusinfo/tilegen/examples/apps/ConfigurationPrinter.scala
@@ -0,0 +1,31 @@
+package com.oculusinfo.tilegen.examples.apps
+
+import com.oculusinfo.tilegen.util.ArgumentParser
+
+import scala.collection.JavaConversions._
+
+
+
+/**
+ * Created by nkronenfeld on 5/21/2015.
+ */
+object ConfigurationPrinter {
+	def main(args: Array[String]): Unit = {
+		val argParser = new ArgumentParser(args)
+		val sc = argParser.getSparkConnector().createContext(Some("Configuration Test"))
+
+		println
+		println
+		println("Spark Configuration:")
+		sc.getConf.getAll.sortBy(_._1).foreach { case (key, value) =>
+			println("\t\"%s\": \"%s\"".format(key, value))
+		}
+
+		println
+		println
+		println("Hadoop configuration:")
+		sc.hadoopConfiguration.iterator().toList.map(entry => (entry.getKey, entry.getValue)).sortBy(_._1).foreach { case (key, value) =>
+			println("\t\"%s\": \"%s\"".format(key, value))
+		}
+	}
+}
