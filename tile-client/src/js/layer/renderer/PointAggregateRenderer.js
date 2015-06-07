53d207b6859dda6a71dffcb04a090c236fb546aa
Scott Langevin <scott.langevin@oculusinfo.com>
2015-06-07 16:35:11 -0400

    Simple support for tooltips on point annotations

--- tile-client/src/js/layer/renderer/PointAggregateRenderer.js
+++ tile-client/src/js/layer/renderer/PointAggregateRenderer.js
@@ -143,7 +143,11 @@
                     html += '<div class="point-annotation point-annotation-fill" style="'
                           + 'left:' + offset.x + 'px;'
                           + 'top:' + offset.y + 'px;'
-                          + 'border-width: 2px;"></div>'
+                          + 'border-width: 2px;"';
+                    if (value[j].data.labels) {
+                        html += ' title="' + value[j].data.labels + '"';
+                    }
+                    html += '></div>'
                           + '<div class="point-annotation point-annotation-border" style="'
                           + 'left:' + offset.x + 'px;'
                           + 'top:' + offset.y + 'px;'
