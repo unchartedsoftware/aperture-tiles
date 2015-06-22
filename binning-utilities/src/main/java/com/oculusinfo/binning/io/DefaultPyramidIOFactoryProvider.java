46c2d45cd251703e8dbcb792775afc5945e83bb1
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2015-06-22 14:59:36 -0400

    Factories for slice-and-dice serializer

--- binning-utilities/src/main/java/com/oculusinfo/binning/io/DefaultPyramidIOFactoryProvider.java
+++ binning-utilities/src/main/java/com/oculusinfo/binning/io/DefaultPyramidIOFactoryProvider.java
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2014 Oculus Info Inc. http://www.oculusinfo.com/
- * 
+ *
  * Released under the MIT License.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,11 +27,7 @@ package com.oculusinfo.binning.io;
 
 import java.util.List;
 
-import com.oculusinfo.binning.io.impl.DummyPyramidIOFactory;
-import com.oculusinfo.binning.io.impl.HBasePyramidIOFactory;
-import com.oculusinfo.binning.io.impl.JDBCPyramidIOFactory;
-import com.oculusinfo.binning.io.impl.SQLitePyramidIOFactory;
-import com.oculusinfo.binning.io.impl.FileBasedPyramidIOFactory;
+import com.oculusinfo.binning.io.impl.*;
 import com.oculusinfo.factory.ConfigurableFactory;
 import com.oculusinfo.factory.providers.FactoryProvider;
 
@@ -42,13 +38,13 @@ import com.oculusinfo.factory.providers.FactoryProvider;
  * availables in the system.<br>
  * <br>
  * To create one use the create method for the desired type. Example:<br>
- * 
+ *
  * <pre>
  * <code>
  * DefaultPyramidIOFactoryProvider.FILE.create();
  * </code>
  * </pre>
- * 
+ *
  * @author cregnier
  */
 public enum DefaultPyramidIOFactoryProvider implements FactoryProvider<PyramidIO> {
@@ -56,6 +52,12 @@ public enum DefaultPyramidIOFactoryProvider implements FactoryProvider<PyramidIO
 			@Override
 			public ConfigurableFactory<PyramidIO> create(ConfigurableFactory<?> parent, java.util.List<String> path) {
 				return new HBasePyramidIOFactory(parent, path);
+			}
+		}),
+	HBASE_SLICED(new Constructor() {
+			@Override
+			public ConfigurableFactory<PyramidIO> create(ConfigurableFactory<?> parent, java.util.List<String> path) {
+				return new HBaseSlicedPyramidIOFactory(parent, path);
 			}
 		}),
     FILE(new Constructor() {
