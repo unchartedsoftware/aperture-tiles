887f13cc6bd4c6625b4425917ce69657a5f66d80
U-OCULUS\nkronenfeld <nkronenfeld@8570w-pc.oculus.local>
2015-06-27 10:42:53 -0400

    More complete, better tests for my sample sliced tile - test contents, limit tests to legitimate slices.

--- binning-utilities/src/test/java/com/oculusinfo/binning/io/impl/HBaseSlicedPyramidIOTest.java
+++ binning-utilities/src/test/java/com/oculusinfo/binning/io/impl/HBaseSlicedPyramidIOTest.java
@@ -23,19 +23,23 @@
  */
 package com.oculusinfo.binning.io.impl;
 
+import com.oculusinfo.binning.BinIndex;
 import com.oculusinfo.binning.TileData;
 import com.oculusinfo.binning.TileIndex;
 import com.oculusinfo.binning.impl.DenseTileData;
+import com.oculusinfo.binning.impl.SparseTileData;
 import com.oculusinfo.binning.io.serialization.TileSerializer;
 import com.oculusinfo.binning.io.serialization.impl.KryoSerializer;
 import com.oculusinfo.binning.io.serialization.impl.PrimitiveArrayAvroSerializer;
 import com.oculusinfo.binning.util.TypeDescriptor;
+import com.oculusinfo.factory.util.Pair;
 import org.apache.avro.file.CodecFactory;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class HBaseSlicedPyramidIOTest {
@@ -79,23 +83,47 @@ public class HBaseSlicedPyramidIOTest {
 
 	@Test
 	public void testRelativeReadSpeed () throws Exception {
+		int iterations = 100;
 		HBaseSlicedPyramidIO io = new HBaseSlicedPyramidIO("hadoop-s1", "2181", "hadoop-s1:60000");
 		TileSerializer<List<Integer>> serializer = new PrimitiveArrayAvroSerializer<>(Integer.class, CodecFactory.nullCodec());
 		String table = "heatmapTimeDebug-sliced";
 		TileIndex index = new TileIndex(0, 0, 0);
 		List<TileIndex> indices = Arrays.asList(index);
 
+		System.out.println("Reading full tile");
+		TileData<List<Integer>> full = null;
 		long startFull = System.currentTimeMillis();
-		for (int i=0; i<256; ++i) {
-			io.readTiles(table, serializer, indices);
+		for (int i=0; i<iterations; ++i) {
+			for (int s=0; s<14; ++s) {
+				full = io.readTiles(table, serializer, indices).get(0);
+			}
 		}
 		long endFull = System.currentTimeMillis();
 
+		System.out.println("Checking slice contents");
+		for (int s=0; s<14; ++s) {
+			TileData<List<Integer>> slice = io.readTiles(table + "["+s+"]", serializer, indices).get(0);
+			for (int x=0; x<full.getDefinition().getXBins(); ++x) {
+				for (int y=0; y<full.getDefinition().getYBins(); ++y) {
+					List<Integer> fullBin = full.getBin(x, y);
+					List<Integer> sliceBin = slice.getBin(x, y);
+					if (null == fullBin || 0 == fullBin.size()) {
+						Assert.assertTrue(null == sliceBin || 0 == sliceBin.size());
+					} else {
+						Assert.assertEquals(14, fullBin.size());
+						Assert.assertEquals(1, sliceBin.size());
+						Assert.assertEquals(fullBin.get(s), sliceBin.get(0));
+					}
+				}
+			}
+		}
+
+		System.out.println("Reading sliced tiles");
 		long startSlice = System.currentTimeMillis();
-		for (int i=0; i<256; ++i) {
-			List<TileData<List<Integer>>> slices = io.readTiles(table + "["+i+"]", serializer, indices);
-			TileData<List<Integer>> slice = slices.get(0);
-			int bins = slice.getDefinition().getXBins();
+		for (int i=0; i<iterations; ++i) {
+			for (int s=0; s<14; ++s) {
+				io.readTiles(table + "["+s+"]", serializer, indices);
+			}
 		}
 		long endSlice = System.currentTimeMillis();
 
