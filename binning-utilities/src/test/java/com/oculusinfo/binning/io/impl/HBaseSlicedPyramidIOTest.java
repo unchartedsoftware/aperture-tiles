c7ce06ccdced19fdd6bb9c7e016f14b29df268c2
U-OCULUS\nkronenfeld <nkronenfeld@8570w-pc.oculus.local>
2015-07-06 14:19:47 -0400

    Some tests of a weird tile set, to make sure it was built properly

--- binning-utilities/src/test/java/com/oculusinfo/binning/io/impl/HBaseSlicedPyramidIOTest.java
+++ binning-utilities/src/test/java/com/oculusinfo/binning/io/impl/HBaseSlicedPyramidIOTest.java
@@ -24,10 +24,12 @@
 package com.oculusinfo.binning.io.impl;
 
 import com.oculusinfo.binning.BinIndex;
+import com.oculusinfo.binning.TileAndBinIndices;
 import com.oculusinfo.binning.TileData;
 import com.oculusinfo.binning.TileIndex;
 import com.oculusinfo.binning.impl.DenseTileData;
 import com.oculusinfo.binning.impl.SparseTileData;
+import com.oculusinfo.binning.io.PyramidIO;
 import com.oculusinfo.binning.io.serialization.TileSerializer;
 import com.oculusinfo.binning.io.serialization.impl.KryoSerializer;
 import com.oculusinfo.binning.io.serialization.impl.PrimitiveArrayAvroSerializer;
@@ -35,13 +37,28 @@ import com.oculusinfo.binning.io.serialization.impl.PrimitiveAvroSerializer;
 import com.oculusinfo.binning.util.TypeDescriptor;
 import com.oculusinfo.factory.util.Pair;
 import org.apache.avro.file.CodecFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.filter.FirstKeyOnlyFilter;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Ignore
 public class HBaseSlicedPyramidIOTest {
@@ -258,5 +275,197 @@ public class HBaseSlicedPyramidIOTest {
 		}
 		Assert.assertTrue(pDiffs, 0 == pDiffs.length());
 		Assert.assertTrue(iDiffs, 0 == iDiffs.length());
+	}
+
+
+	@Test
+	public void testDenseSlicedTiles () throws Exception {
+		TileIndex index = new TileIndex(0, 0, 0, 4, 4);
+		TileData<List<Integer>> tile = new DenseTileData<>(index);
+		for (int x=0; x<4; ++x) {
+			for (int y=0; y<4; ++y) {
+				List<Integer> bin = new ArrayList<>();
+				for (int z=0; z<4; ++z) {
+					bin.add(x+y+z);
+				}
+				tile.setBin(x, y, bin);
+			}
+		}
+
+		String table = "dense-bucket-test";
+		HBaseSlicedPyramidIO io = new HBaseSlicedPyramidIO("hadoop-s1", "2181", "hadoop-s1:60000");
+		TileSerializer<List<Integer>> serializer = new KryoSerializer<>(new TypeDescriptor(List.class, new TypeDescriptor(Integer.class)));
+
+		io.initializeForWrite(table);
+		io.writeTiles(table, serializer, Arrays.asList(tile));
+
+		List<TileIndex> indices = Arrays.asList(index);
+		TileData<List<Integer>> full = io.readTiles(table, serializer, indices).get(0);
+		TileData<List<Integer>> s0 = io.readTiles(table+"[0]", serializer, indices).get(0);
+		TileData<List<Integer>> s1 = io.readTiles(table+"[1]", serializer, indices).get(0);
+		TileData<List<Integer>> s2 = io.readTiles(table+"[2]", serializer, indices).get(0);
+		TileData<List<Integer>> s3 = io.readTiles(table+"[3]", serializer, indices).get(0);
+		List<TileData<List<Integer>>> slices = Arrays.asList(s0, s1, s2, s3);
+		TileData<List<Integer>> s01 = io.readTiles(table+"[0-1]", serializer, indices).get(0);
+		TileData<List<Integer>> s23 = io.readTiles(table+"[2-3]", serializer, indices).get(0);
+		List<TileData<List<Integer>>> slicePairs = Arrays.asList(s01, s23);
+
+		for (int x=0; x<4; ++x) {
+			for (int y=0; y<4; ++y) {
+				for (int z=0; z<4; ++z) {
+					Assert.assertEquals(tile.getBin(x, y).get(z), full.getBin(x, y).get(z));
+					Assert.assertEquals(tile.getBin(x, y).get(z), slices.get(z).getBin(x, y).get(0));
+					Assert.assertEquals(tile.getBin(x, y).get(z), slicePairs.get(z/2).getBin(x, y).get(z%2));
+				}
+			}
+		}
+	}
+
+	// List out all indices in the given weird tile set, for use in the next test
+	@Ignore
+	public void listIndices () throws Exception {
+		String tableName = "nycHeatmap_sw2015_sliced_DELETEME";
+		String zookeeperQuorum = "hadoop-s1";
+		String zookeeperPort = "2181";
+		String hbaseMaster = "hadoop-s1:60000";
+
+		Configuration config = HBaseConfiguration.create();
+		config.set("hbase.zookeeper.quorum", zookeeperQuorum);
+		config.set("hbase.zookeeper.property.clientPort", zookeeperPort);
+		config.set("hbase.master", hbaseMaster);
+		config.set("hbase.client.keyvalue.maxsize", "0");
+		Connection connection = ConnectionFactory.createConnection(config);
+		Admin admin = connection.getAdmin();
+		Table table = connection.getTable(TableName.valueOf(tableName));
+
+		System.out.println("scanning full table:");
+		Scan scan = new Scan();
+		scan.setFilter(new FirstKeyOnlyFilter());
+		ResultScanner scanner = table.getScanner(scan);
+		for (Result rr : scanner) {
+			byte[] key = rr.getRow();
+			String keyName = new String(key);
+			System.out.println(keyName);
+		}
+	}
+
+	private <T> Map<TileIndex, TileData<T>> getTileMap (PyramidIO io, TileSerializer<T> serializer,
+														String table, Integer slice, List<TileIndex> indices) throws Exception {
+		if (null != slice) {
+			table = table + "[" + slice + "]";
+		}
+		List<TileData<T>> tiles = io.readTiles(table, serializer, indices);
+		Map<TileIndex, TileData<T>> result = new HashMap<>();
+		for (TileData<T> tile: tiles) {
+			result.put(tile.getDefinition(), tile);
+		}
+		return result;
+	}
+	List<Integer> addLists (List<Integer> a, List<Integer> b) {
+		if (null == a || 0 == a.size()) return b;
+		if (null == b || 0 == b.size()) return a;
+		List<Integer> c = new ArrayList<>();
+		int N = Math.max(a.size(), b.size());
+		for (int n=0; n<N; ++n) {
+			int v = 0;
+			if (n < a.size()) v += a.get(n);
+			if (n < b.size()) v += b.get(n);
+			c.add(v);
+		}
+		return c;
+	}
+
+	// Make sure the various levels match in a weird tile set - i.e., make sure there are no lower-level holes that
+	// don't also exist at upper levels.
+	@Test
+	public void testWeirdTable () throws Exception {
+		String table = "nycHeatmap_sw2015_sliced_DELETEME";
+		HBaseSlicedPyramidIO io = new HBaseSlicedPyramidIO("hadoop-s1", "2181", "hadoop-s1:60000");
+		TileSerializer<List<Integer>> serializer = new KryoSerializer<>(new TypeDescriptor(List.class, new TypeDescriptor(Integer.class)));
+
+		for (int slice = 0; slice < 53; ++slice) {
+			Map<TileIndex, TileData<List<Integer>>> level8 =
+				getTileMap(io, serializer, table, slice,
+					Arrays.asList(new TileIndex(8, 75, 159)));
+			Map<TileIndex, TileData<List<Integer>>> level9 =
+				getTileMap(io, serializer, table, slice,
+					Arrays.asList(new TileIndex(9, 150, 319), new TileIndex(9, 151, 319)));
+			Map<TileIndex, TileData<List<Integer>>> level10 =
+				getTileMap(io, serializer, table, slice,
+					Arrays.asList(new TileIndex(10, 300, 638), new TileIndex(10, 301, 638),
+						new TileIndex(10, 301, 639), new TileIndex(10, 302, 638),
+						new TileIndex(10, 302, 639)));
+			Map<TileIndex, TileData<List<Integer>>> level11 =
+				getTileMap(io, serializer, table, slice,
+					Arrays.asList(new TileIndex(11, 601, 1276), new TileIndex(11, 601, 1277),
+						new TileIndex(11, 602, 1276), new TileIndex(11, 602, 1277),
+						new TileIndex(11, 602, 1278), new TileIndex(11, 603, 1276),
+						new TileIndex(11, 603, 1277), new TileIndex(11, 603, 1278),
+						new TileIndex(11, 603, 1279), new TileIndex(11, 604, 1276),
+						new TileIndex(11, 604, 1277), new TileIndex(11, 604, 1278),
+						new TileIndex(11, 604, 1279)));
+			Map<TileIndex, TileData<List<Integer>>> level12 =
+				getTileMap(io, serializer, table, slice,
+					Arrays.asList(new TileIndex(12, 1202, 2552), new TileIndex(12, 1202, 2553),
+						new TileIndex(12, 1203, 2552), new TileIndex(12, 1203, 2553),
+						new TileIndex(12, 1203, 2554), new TileIndex(12, 1203, 2555),
+						new TileIndex(12, 1204, 2552), new TileIndex(12, 1204, 2553),
+						new TileIndex(12, 1204, 2554), new TileIndex(12, 1204, 2555),
+						new TileIndex(12, 1205, 2554), new TileIndex(12, 1205, 2555),
+						new TileIndex(12, 1205, 2556), new TileIndex(12, 1206, 2553),
+						new TileIndex(12, 1206, 2554), new TileIndex(12, 1206, 2555),
+						new TileIndex(12, 1206, 2556), new TileIndex(12, 1206, 2557),
+						new TileIndex(12, 1206, 2558), new TileIndex(12, 1207, 2553),
+						new TileIndex(12, 1207, 2554), new TileIndex(12, 1207, 2555),
+						new TileIndex(12, 1207, 2556), new TileIndex(12, 1207, 2557),
+						new TileIndex(12, 1207, 2558), new TileIndex(12, 1208, 2553),
+						new TileIndex(12, 1208, 2554), new TileIndex(12, 1208, 2555),
+						new TileIndex(12, 1208, 2556), new TileIndex(12, 1208, 2557),
+						new TileIndex(12, 1208, 2558), new TileIndex(12, 1209, 2554),
+						new TileIndex(12, 1209, 2555), new TileIndex(12, 1209, 2556)));
+
+			Map<Integer, Map<TileIndex, TileData<List<Integer>>>> allTiles = new HashMap<>();
+			allTiles.put(8, level8);
+			allTiles.put(9, level9);
+			allTiles.put(10, level10);
+			allTiles.put(11, level11);
+			allTiles.put(12, level12);
+
+			for (int t = 8; t < 12; ++t) {
+				for (TileData<List<Integer>> topTile : allTiles.get(t).values()) {
+					TileIndex topIdx = topTile.getDefinition();
+					for (int b = t + 1; b < 13; ++b) {
+						int lf = 1 << (b - t);
+						for (int x = 0; x < 256; ++x) {
+							for (int y = 0; y < 256; ++y) {
+								List<Integer> expected = topTile.getBin(x, y);
+								List<Integer> actual = null;
+								BinIndex baseTopUBin = TileIndex.tileBinIndexToUniversalBinIndex(topIdx, new BinIndex(x, y));
+								BinIndex baseUBin = new BinIndex(baseTopUBin.getX() * lf, baseTopUBin.getY() * lf);
+								TileIndex baseTile = new TileIndex(b, 0, 0);
+								for (int rx = 0; rx < lf; ++rx) {
+									for (int ry = 0; ry < lf; ++ry) {
+										BinIndex bin = new BinIndex(baseUBin.getX() + rx, baseUBin.getY() + ry);
+										TileAndBinIndices tbi = TileIndex.universalBinIndexToTileBinIndex(baseTile, bin);
+										TileData<List<Integer>> subTile = allTiles.get(b).get(tbi.getTile());
+										if (null != subTile) {
+											List<Integer> subBin = subTile.getBin(tbi.getBin().getX(), tbi.getBin().getY());
+											actual = addLists(actual, subBin);
+										}
+									}
+								}
+								if (null == expected || 0 == expected.size()) {
+									Assert.assertTrue(null == actual || 0 == actual.size());
+								} else {
+									Assert.assertEquals(expected.size(), actual.size());
+									for (int n = 0; n < expected.size(); ++n)
+										Assert.assertEquals(expected.get(n), actual.get(n));
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
