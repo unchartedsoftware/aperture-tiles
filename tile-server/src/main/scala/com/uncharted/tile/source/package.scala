e5048ec4a48df6c1b0ca913c29a5755461f48a2e
U-OCULUS\nkronenfeld <nkronenfeld@8570w-pc.oculus.local>
2015-07-09 13:24:33 -0400

    Package docs for the tile server project, beginning planning how this will actually work.

---
+++ tile-server/src/main/scala/com/uncharted/tile/source/package.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Uncharted Software Inc.
+ * http://www.oculusinfo.com/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.uncharted.tile
+
+/**
+ * This package, and sub-packages, deals with sourcing tiles from an external server.
+ *
+ * The external server is in the server sub-package, the client that uses it, in the client sub-package.
+ *
+ * The client/server communications for this external server is handles using RabbitMQ, but these communications
+ * should all be encapsulated within the client, so users need not know that (except for configuration details,
+ * of course).
+ */
+package object source {
+
+}
