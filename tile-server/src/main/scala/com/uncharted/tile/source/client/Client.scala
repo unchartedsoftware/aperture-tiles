bc934a1c5b699fd424d86bed3d60be59d90ab827
U-OCULUS\nkronenfeld <nkronenfeld@8570w-pc.oculus.local>
2015-07-15 23:54:22 -0400

    Separate out client code from tile client code Test basic client code

--- tile-server/src/main/scala/com/uncharted/tile/source/client/Client.scala
+++ tile-server/src/main/scala/com/uncharted/tile/source/client/Client.scala
@@ -1,8 +1,89 @@
+/*
+ * Copyright (c) 2015 Uncharted Software Inc.
+ * http://www.oculusinfo.com/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.uncharted.tile.source.client
 
-/**
- * Created by nkronenfeld on 7/14/2015.
- */
-class Client {
 
+
+import java.util.{List, Arrays, UUID}
+
+import com.rabbitmq.client.AMQP.BasicProperties
+import com.rabbitmq.client.QueueingConsumer
+import com.uncharted.tile.source.server.RabbitMQConnectable
+import com.uncharted.tile.source.util.ByteArrayCommunicator
+import grizzled.slf4j.Logging
+
+
+
+/**
+ * A basic, generic client that communicates with our basic, generic server.
+ *
+ * @tparam RT The type of request to be made
+ */
+abstract class Client[RT] (host: String, requestExchange: String) extends RabbitMQConnectable(host) with Logging {
+  import com.uncharted.tile.source.server
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def encodeRequest (request: RT): Array[Byte]
+  def processError (request: RT, severity: String, serverError: Throwable): Unit = {
+    severity match {
+      case server.LOG_ERROR => error("Major server error", serverError)
+      case server.LOG_WARNING => warn("Server error", serverError)
+      case server.LOG_INFO => info("Minor server error", serverError)
+      case server.LOG_DEBUG => debug("Server debug: ", serverError)
+    }
+  }
+  def processResults (request: RT, contentType: String, contents: Array[Byte])
+
+  def makeRequest (request: RT): Unit = {
+    // Create a unique channel on which to listen for responses
+    var replyQueue = UUID.randomUUID.toString
+    // Listen on our response channel for our tiles
+    concurrent.future(new RequestResponseListener(replyQueue, request))
+    // Create our tile request data
+    val encodedRequest = encodeRequest(request)
+
+    // Send our message
+    _channel.basicPublish(requestExchange, "", new BasicProperties.Builder().replyTo(replyQueue).build(), encodeRequest(request))
+  }
+
+  private def onError (request: RT, severity: String, rawError: Array[Byte]): Unit =
+    processError(request, severity, ByteArrayCommunicator.defaultCommunicator.read[Throwable](rawError))
+
+  class RequestResponseListener (queue: String, request: RT) {
+    val consumer = new QueueingConsumer(_channel)
+    _channel.queueDeclare(queue, false, false, false, null)
+    _channel.basicQos(1)
+    _channel.basicConsume(queue, true, consumer)
+    val delivery = consumer.nextDelivery()
+    val contentType = delivery.getProperties.getContentType
+    contentType match {
+      case server.LOG_ERROR => onError(request, server.LOG_ERROR, delivery.getBody)
+      case server.LOG_WARNING => onError(request, server.LOG_WARNING, delivery.getBody)
+      case server.LOG_INFO => onError(request, server.LOG_INFO, delivery.getBody)
+      case server.LOG_DEBUG => onError(request, server.LOG_DEBUG, delivery.getBody)
+      case _ => processResults(request, contentType, delivery.getBody)
+    }
+  }
 }
--- tile-server/src/main/scala/com/uncharted/tile/source/client/TileClient.scala
+++ tile-server/src/main/scala/com/uncharted/tile/source/client/TileClient.scala
@@ -28,6 +28,8 @@ package com.uncharted.tile.source.client
 import java.io.ByteArrayInputStream
 import java.util
 import java.util.{List => JavaList, Arrays => JavaArrays, UUID}
+import com.uncharted.tile.source.server
+
 import scala.collection.JavaConverters._
 
 import com.oculusinfo.binning.TileData
@@ -46,51 +48,27 @@ import com.uncharted.tile.source.util.ByteArrayCommunicator
  * when tiles are completed (or have errored).
  */
 class TileClient(host: String,
-                 serializerFactoryProvider: FactoryProvider[TileSerializer[_]]) extends RabbitMQConnectable(host) {
-  import com.uncharted.tile.source.server
-  import scala.concurrent.ExecutionContext.Implicits.global
+                 serializerFactoryProvider: FactoryProvider[TileSerializer[_]])
+extends Client[ClientTileRequest[_]](host, server.TILE_REQUEST_EXCHANGE) {
+  def encodeRequest(request: ClientTileRequest[_]): Array[Byte] =
+    ServerTileRequest.toByteArray(request)
 
-  def requestTile[T] (request: ClientTileRequest[T]): Unit = {
-    // Create a unique channel on which to listen for responses
-    var replyQueue = UUID.randomUUID.toString
-    // Listen on our response channel for our tiles
-    concurrent.future(new TileRequestResponseListener(replyQueue, request))
-    // Create our tile request data
-    val postableRequest = ServerTileRequest.toByteArray(request)
+  def processResults(request: ClientTileRequest[_], contentType: String, contents: Array[Byte]) =
+    processResultsInternal(request, contentType, contents)
 
-    // Send our message
-    _channel.basicPublish(server.TILE_REQUEST_EXCHANGE, "", new BasicProperties.Builder().replyTo(replyQueue).build(), postableRequest)
-  }
+  def processResultsInternal[T] (request: ClientTileRequest[T], contentType: String, contents: Array[Byte]): Unit = {
+    // Get a serializer to deserialize our results
+    val tsFactory = serializerFactoryProvider.createFactory("", null, JavaArrays.asList[String]())
+    tsFactory.readConfiguration(request.configuration)
+    val serializer = tsFactory.produce(classOf[TileSerializer[T]])
 
-  class TileRequestResponseListener[T] (queue: String, request: ClientTileRequest[T]) {
-    val consumer = new QueueingConsumer(_channel)
-    _channel.queueDeclare(queue, false, false, false, null)
-    _channel.basicQos(1)
-    _channel.basicConsume(queue, true, consumer)
-    val delivery = consumer.nextDelivery()
-    val contentType = delivery.getProperties.getContentType
-    contentType match {
-      case server.TILE => {
-        // Get a serializer to deserialize our results
-        val tsFactory = serializerFactoryProvider.createFactory("", null, JavaArrays.asList[String]())
-        tsFactory.readConfiguration(request.configuration)
-        val serializer = tsFactory.produce(classOf[TileSerializer[T]])
-
-        // Extract our results
-        val encodedTiles = ByteArrayCommunicator.defaultCommunicator.read[JavaList[Array[Byte]]](delivery.getBody)
-        val tiles = new util.ArrayList[TileData[T]]()
-        for (n <- 0 until request.indices.size()) {
-          val bais = new ByteArrayInputStream(encodedTiles.get(n))
-          tiles.add(serializer.deserialize(request.indices.get(n), bais))
-        }
-        request.onTileRetrieved(tiles)
-      }
-      case _ => {
-        // Must be an error.
-        val severity = contentType
-        val error = ByteArrayCommunicator.defaultCommunicator.read[Throwable](delivery.getBody)
-        request.onError(error)
-      }
+    // Extract our results
+    val encodedTiles = ByteArrayCommunicator.defaultCommunicator.read[JavaList[Array[Byte]]](contents)
+    val tiles = new util.ArrayList[TileData[T]]()
+    for (n <- 0 until request.indices.size()) {
+      val bais = new ByteArrayInputStream(encodedTiles.get(n))
+      tiles.add(serializer.deserialize(request.indices.get(n), bais))
     }
+    request.onTileRetrieved(tiles)
   }
 }
