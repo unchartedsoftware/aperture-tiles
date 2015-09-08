d79e607959e464467453a11fb95ca41a8e7eabdd
Nathan Kronenfeld <nkronenfeld@oculusinfo.com>
2015-09-08 13:08:08 -0400

    Add user parameters to start-server script

--- tile-provider/server/start-server.sh
+++ tile-provider/server/start-server.sh
@@ -6,4 +6,5 @@ MAIN_JAR=build/libs/server-0.7-SNAPSHOT-cdh5.4.1-assembly.jar
 JOB_MASTER=yarn-client
 
 spark-submit --num-executors 12 --executor-memory 10g --executor-cores 4 \
-	--master ${JOB_MASTER} --class ${MAIN_CLASS} ${MAIN_JAR}
+	--master ${JOB_MASTER} --class ${MAIN_CLASS} ${MAIN_JAR} \
+	-rabbitMQHost localhost -rabbitMQUser test -rabbitMQPassword test
