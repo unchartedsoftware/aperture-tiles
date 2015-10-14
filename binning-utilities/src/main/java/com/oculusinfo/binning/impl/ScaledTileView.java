5a3c8d863303004ceb072e3dbad1f9ef0375139c
wmayo <wmayo@uncharted.software>
2015-10-14 10:07:08 -0400

    Added license headers to new files

--- binning-utilities/src/main/java/com/oculusinfo/binning/impl/ScaledTileView.java
+++ binning-utilities/src/main/java/com/oculusinfo/binning/impl/ScaledTileView.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2015 Uncharted Software. http://www.uncharted.software/
+ *
+ * Released under the MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.oculusinfo.binning.impl;
 
 import com.oculusinfo.binning.TileData;
@@ -8,7 +31,8 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Allows you to scale tiles with bins containing a smaller number of values to one with a greater number of values
+ * Implementation of TileData that scales the number of bin values to a desired bin size by making duplicates
+ * of the value where needed. Allows operations to be performed on two datasets with a different number of bin values.
  */
 public class ScaledTileView<T extends Number> implements TileData<List<T>> {
 
@@ -46,7 +70,7 @@ public class ScaledTileView<T extends Number> implements TileData<List<T>> {
 			count += multiple;
 
 			while (count > 1) {
-				results.add(bin.get(i));	// TODO: Instead of just copying values may want to interpolate between data points
+				results.add(bin.get(i));
 				count -= 1.0;
 			}
 		}
